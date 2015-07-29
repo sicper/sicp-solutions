9b6ffca96315d1ceb0e8e0c96669279c64e394ba
chenhao <chenhao@qima-inc.com>
2015-07-29 10:42:28 +0800

    change lib

--- chenhao/libs/common-lib.scm
+++ chenhao/libs/common-lib.scm
@@ -1,6 +1,13 @@
 (define (add-list items item)
  (cons item items ))
 
-
-
 (define nil '())
+(define (append a b)
+  (if (null? a)
+       b
+      (cons (car a) (append (cdr a) b))))
+
+(define (reverse l)
+  (if (null? l)
+       nil
+      (append (cdr l) (cons (car l) nil))))
